248a2dca5e1d5799e72b546115001db6d97fcc65
Anjana Sreekanth <anjanasreekanth@outlook.com>
2026-05-26 17:50:43 -0500

    Log square of numbers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // Anjana Sreekanth - 05-26-2026
-// Prints Hello world! 
-//This is the change from new branch
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
+    console.log(i * i);
 }
