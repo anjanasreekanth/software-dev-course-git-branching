7342c7786b28106eaad05c5c4b1b2f23ca3c238b
Anjana Sreekanth <anjanasreekanth@outlook.com>
2026-05-26 17:52:15 -0500

    Update author comment with purpose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // Anjana Sreekanth - 05-26-2026
-// Prints Hello world! 
-//This is the change from new branch
+// This application prints "Hello, World!" and the numbers 0 to 9.
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
